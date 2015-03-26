478fa086e0b08f44137ea8c1483d85206fcf8b6c
Danniel Condez <ducondez@gmail.com>
2015-03-26 09:06:09 -0700

    setup karma test runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,34 @@
 var gulp = require('gulp'),
-	babel = require('gulp-babel'),
-	watch = require('gulp-watch'),
-	mocha = require('gulp-mocha'),
-	concat = require('gulp-concat');
+  babel = require('gulp-babel'),
+  watch = require('gulp-watch'),
+  karma = require('karma').server,
+  concat = require('gulp-concat');
 
 gulp.task('default', function() {
-	gulp.run('test');
-	gulp.run('babel');
+  gulp.run('test');
+  gulp.run('babel');
 });
 
 gulp.task('babel', function() {
-	return gulp.src('src/**/*.js')
-		.pipe(babel())
-		.pipe(concat('gabar.js'))
-		.pipe(gulp.dest('dist'));
+  return gulp.src('src/**/*.js')
+    .pipe(babel())
+    .pipe(concat('gabar.js'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('test', function () {
-	return gulp.src('test/**/*.js', {read: false}).
-		pipe(mocha({reporter: 'spec'}));
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
 });
 
 gulp.task('watch', function () {
-	watch(['src/**/*.js', 'test/**/*.js'],  function () {
-		gulp.run('test');
-		gulp.run('babel');
-	});
+  watch(['src/**/*.js', 'test/**/*.js'],  function () {
+    karma.start({
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: false
+    });
+    gulp.run('babel');
+  });
 });
--- test/test-app.js
+++ test/test-app.js
@@ -1,9 +1,8 @@
-var assert = require('assert');
 
 describe("Array", function() {
   describe("#indexOf", function() {
     it('should return -1 when value not present', function() {
-    	assert.equal(-1, [0].indexOf(5));
+      	expect(true).toEqual(true)
     });
   });
 });
